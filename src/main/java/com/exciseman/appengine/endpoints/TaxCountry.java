f7982d38fa9a10ce85f01277da3cc2c35f63c9ae
bruce <bruce@moo.com>
2016-08-20 16:09:53 +0100

    3

--- src/main/java/com/exciseman/appengine/endpoints/TaxCountry.java
+++ src/main/java/com/exciseman/appengine/endpoints/TaxCountry.java
@@ -1,27 +1,33 @@
 package com.exciseman.appengine.endpoints;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
-
+import com.exciseman.country.State;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-
-/**
- * Created by bruce on 20/08/16.
- */
 public class TaxCountry extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         PrintWriter out = response.getWriter();
-        out.println("This is Tax Country endpoint, Dudes");
+
+        ServletContext context = getServletContext();
+        String fullPath = context.getRealPath("usa.json");
+        InputStream resourceContent = context.getResourceAsStream(fullPath.trim());
+
+        try (Reader reader = new InputStreamReader(resourceContent)) {
+            Gson gson = new GsonBuilder().create();
+            State state = gson.fromJson(reader, State.class);
+            out.println(state);
+        } catch (Exception e) {
+            out.println("Failure");
+            out.println(e.getLocalizedMessage());
+        }
     }
+
 }
--- src/main/java/com/exciseman/country/State.java
+++ src/main/java/com/exciseman/country/State.java
@@ -3,15 +3,15 @@ package com.exciseman.country;
 /**
  * Created by bruce on 20/08/16.
  */
-public class Country {
+public class State {
     private String name;
-    private String abbrev;
+    private String abbreviation;
 
     // Getters and setters are not required for this example.
     // GSON sets the fields directly using reflection.
 
     @Override
     public String toString() {
-        return name + " - " + abbrev;
+        return name + " - " + abbreviation;
     }
 }
